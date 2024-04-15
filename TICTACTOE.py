155d0c8e8a398cd01f4eae80d9120f56c00947a5
Donavan <aladslack@gmail.com>
2024-04-15 03:21:14 -0500

    MADE PRINT_BOARD FUNCTION THAT PRINTS THE TIC TAC TOE BOARD.

--- TICTACTOE.py
+++ TICTACTOE.py
@@ -1,1 +1,14 @@
 
+def print_board(board):
+    
+    "Prints Tic Tac Toe board."
+    
+    for row in board:
+        print (" | ".join(row))
+        print ("-" * 5)
+    return 0
+
+board = [[' ' for _ in range(3)] for _ in range(3)]
+players = ['X', '0']
+
+print_board(board)
