eb51fde225a7db8365e33eddf013331cb15016c2
Donavan <aladslack@gmail.com>
2024-04-15 11:41:30 -0500

    ADDED NEW FUNCTION TO TICTACTOE UPDATED README

--- TICTACTOE.py
+++ TICTACTOE.py
@@ -2,12 +2,24 @@ import random
 
 def print_board(board):
     
-    "Prints Tic Tac Toe board."
+    #Prints Tic Tac Toe board.
     
     for row in board:
         print (" | ".join(row))
         print ("-" * 5)
     return 0
+
+def check_winner(board):
+    
+    #Checks if there's a winner
+    
+    for row in board:
+        if len(set(row)) ==1 and row[0] != ' ':
+            return row[0]
+        
+    for col in range(3):
+        if len(set([board[row][col] for row in range(3)])) == 1 and board[0][col] != ' ':
+            return board[0][col]
 
 board = [[' ' for _ in range(3)] for _ in range(3)]
 players = ['X', '0']
